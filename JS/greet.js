fedce43609ceb813004328a6ee4bcb74e9fe09e7
LuyoloLuja <luyolom19@gmail.com>
2020-06-18 17:13:08 +0200

    fixed my style

--- JS/greet.js
+++ JS/greet.js
@@ -4,19 +4,17 @@ var greetingCounter = document.querySelector(".counter");
 var greetingMessage = document.querySelector(".greeting");
 var count = 1;
 
-function GreetMe(){
+function GreetMe() {
     var languages = document.querySelector("input[name='language']:checked");
     var languageChecked = languages.value;
     var username = theName.value;
 
-    if(languageChecked){
-        if(languageChecked === "english"){
-            greetingMessage.innerHTML = "Hi, " + username;
-        } else if(languageChecked === "afrikaans"){
-            greetingMessage.innerHTML = "More, " + username;
-        } else if(languageChecked === "isixhosa"){
-            greetingMessage.innerHTML = "Molo, " + username;
-        }
+    if (languageChecked === "english") {
+        greetingMessage.innerHTML = "Hi, " + username;
+    } else if (languageChecked === "afrikaans") {
+        greetingMessage.innerHTML = "More, " + username;
+    } else if (languageChecked === "isixhosa") {
+        greetingMessage.innerHTML = "Molo, " + username;
     }
     languageChecked.value = count;
     greetingCounter.innerHTML = count++;
@@ -26,6 +24,6 @@ function GreetMe(){
 myButton.addEventListener('click', GreetMe);
 
 // clears the text box when button is clicked
-function ClearTextbox(){
+function ClearTextbox() {
     document.querySelector(".theName").value = " ";
 }
